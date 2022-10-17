56683c536d384508dab88bc5c482a8968acba1a9
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-10-17 15:34:10 -0400

    completed assignment

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+        public static double Area(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int quotient = (a / b);
+            int remainder = (a % b);
+            double radius;
+
+            Console.WriteLine($"{a}+{b} is {a + b}");
+            Console.WriteLine($"{a}-{b} is {a - b}");
+            Console.WriteLine($"{a}*{b} is {a * b}");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What is the radius of your circle?");
+            radius = double.Parse(Console.ReadLine());
+
+            //double area = Math.PI * Math.Pow(radius, 2);
+            double area = Area(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}.");
         }
     }
 }
